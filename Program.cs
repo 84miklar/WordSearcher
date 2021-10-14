35582d8d62af1bbbcc769af3e548a07f72087676
Svante-J <72524651+Svante-J@users.noreply.github.com>
2021-10-14 16:43:29 +0200

    Ny branch. Lite Todo kommentarer

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace WordSearcher
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //Todo: gå från program till class som sköter logik (start tom)
+            //todo: Börja göra mappstruktur
+            //todo: Menyer /switch eller if satser
+            //todo: Läsa in .txt till lista
+            
         }
     }
 }
