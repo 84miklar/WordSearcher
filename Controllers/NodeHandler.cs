a844c0e7340f5eca21595905cd36660b0633a0e4
Mikael Larsson <mikael_larsson84@hotmail.com>
2021-10-15 14:16:12 +0200

    Added some classes

---
+++ Controllers/NodeHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSearcher.Controllers
+{
+    class NodeHandler
+    {
+        public void SearchData()
+        {
+
+        }
+        public void SaveData()
+        {
+
+        }
+        public void OutputData()
+        {
+            //En loop som hämtar data och skickar till view.
+        }
+    }
+}
---
+++ View/NodeHandlerView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSearcher.View
+{
+    class NodeHandlerView
+    {
+        public void OutputView()
+        {
+            //Console.WriteLine("");
+        }
+    }
+}
