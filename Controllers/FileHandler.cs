2f0ab4c7be1c7a3baa02bb07e2b6ec2f7f56c9f8
Svante-J <72524651+Svante-J@users.noreply.github.com>
2021-10-15 16:20:38 +0200

    readread

--- Controllers/FileHandler.cs
+++ Controllers/FileHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,15 @@ namespace WordSearcher.Controller
     {
         public static void GetDataFromTexFile()
         {
-            string text = System.IO.File.ReadAllText(@"..\..\WordSearcher\Recourses\Readtest.txt");
+
+
+           // var path = Path.Combine(Directory.GetCurrentDirectory(), "/Readtest.txt");
+            //Console.WriteLine(path.ToString());
+            string text = System.IO.File.ReadAllText("/Readtest.txt");
            
             System.Console.WriteLine("Contents of ReadTest.txt = {0}", text);
-                
 
+            
         }
     }
 }
