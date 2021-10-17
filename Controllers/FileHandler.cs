5669159eb9bbcdaaf113618037cb59b4c228f2c0
Svante-J <72524651+Svante-J@users.noreply.github.com>
2021-10-17 20:26:53 +0200

    bråkade lite med full path

--- Controllers/FileHandler.cs
+++ Controllers/FileHandler.cs
@@ -13,10 +13,29 @@ namespace WordSearcher.Controller
         {
 
 
-           // var path = Path.Combine(Directory.GetCurrentDirectory(), "/Readtest.txt");
+            // var path = Path.Combine(Directory.GetCurrentDirectory(), "/Readtest.txt");
             //Console.WriteLine(path.ToString());
-            string text = System.IO.File.ReadAllText("/Readtest.txt");
-           
+            string fileName = "Readtest.txt";
+            string path1 = @"Controllers";
+            string path2 = @"\Controllers";
+            string fullPath;
+
+            fullPath = Path.GetFullPath(path1);
+            Console.WriteLine("GetFullPath('{0}') returns '{1}'",
+                path1, fullPath);
+
+            //fullPath = Path.GetFullPath(fileName);
+            //Console.WriteLine("GetFullPath('{0}') returns '{1}'",
+            //fileName, fullPath);
+
+            //fullPath = Path.GetFullPath(path2);
+            //Console.WriteLine("GetFullPath('{0}') returns '{1}'",
+            //    path2, fullPath);
+            //Path.GetDirectoryName
+
+            string text = System.IO.File.ReadAllText(fullPath +"/Readtest.txt");
+            //C:\Users\svanths\Source\Repos\84miklar\WordSearcher\Controllers\Readtest.txt
+
             System.Console.WriteLine("Contents of ReadTest.txt = {0}", text);
 
             
