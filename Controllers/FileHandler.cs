c8ed0ca4349e3213b7f3173b2e13da4676c9a176
Mikael Larsson <mikael_larsson84@hotmail.com>
2021-10-21 13:26:09 +0200

    added RemoveAllButLetters

--- Controllers/FileHandler.cs
+++ Controllers/FileHandler.cs
@@ -12,14 +12,17 @@ namespace WordSearcher.Controller
         {
             var fileName = "c#1000.txt";
             var text = System.IO.File.ReadAllText(fileName);
+            text = RemoveAllButLetters(text);
             var text1 = new Arrays(MakeTextIntoArray(text), fileName);
             AddTextArrayToList(text1);
             fileName = "Computer programming1500.txt";
             text = System.IO.File.ReadAllText(fileName);
+            text = RemoveAllButLetters(text);
             var text2 = new Arrays(MakeTextIntoArray(text), fileName);
             AddTextArrayToList(text2);
             fileName = "javascript3000.txt";
             text = System.IO.File.ReadAllText(fileName);
+            text = RemoveAllButLetters(text);
             var text3 = new Arrays(MakeTextIntoArray(text), fileName);
             AddTextArrayToList(text3);
         }
@@ -27,12 +30,27 @@ namespace WordSearcher.Controller
         public static string[] MakeTextIntoArray(string text)
         {
             //  var separators = new char[] { ' ', '.', ',', '[', ']', '(', ')', '"', '*'};
-            var separators = new string[] { " ", ".",",", "[", "]", "(", ")", "\"" , "*", "\n","\r","\t", "/", "{", "}","-", "#" };
+            //var separators = new string[] { " ", ".",",", "[", "]", "(", ")", "\"" , "*", "\n","\r","\t", "/", "{", "}","-", "#" };
+            var separators = new char[] { ' ', '\n', '\r', '\t'};
             return text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
         }
         private static void AddTextArrayToList(Arrays textArray)
         {
             Arrays.ArrayList.Add(textArray);
+        }
+        public static string RemoveAllButLetters(string text)
+        {
+            var newString = "";
+            var builder = new System.Text.StringBuilder();
+            builder.Append(newString);
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsLetter(text[i]) || Char.IsWhiteSpace(text[i]))
+                {
+                    builder.Append(text[i]);
+                }
+            }
+            return builder.ToString();
         }
     }
 }
